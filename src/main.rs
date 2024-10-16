38fe61330f90cb2a16fc6bee2f0e3aded8a6dd12
Lorenzo Borelli <lorenzo.borelli27@gmail.com>
2024-10-16 21:51:06 +0200

    downloading model from hf

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 use clap::Parser;
+use hf_hub::api::tokio::Api;
+use candle_core::Device;
+use std::any::type_name;
+use tokio;
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -12,10 +20,15 @@ struct Args {
     n_images: u8,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
+    let api = Api::new().unwrap();
+    let repo = api.model("stable-diffusion-v1-5/stable-diffusion-v1-5".to_string());
+    let weights = repo.get("v1-5-pruned.safetensors").await.unwrap();
+    let weights = candle_core::safetensors::load(weights, &Device::Cpu);
 
-
-    println!("Hello, world!");
+    println!("Model weights loaded!");
+    println!("{:?}", print_type_of(&weights))
 }
