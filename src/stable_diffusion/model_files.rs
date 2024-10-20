4a64c93095314b4d0fbaca49d75c668d2cf5ff18
Lorenzo Borelli <lorenzo.borelli27@gmail.com>
2024-10-20 11:05:57 +0200

    added more unit tests

--- src/stable_diffusion/model_files.rs
+++ src/stable_diffusion/model_files.rs
@@ -1,6 +1,3 @@
-
-
-
 #[derive(Debug)]
 pub enum ModelFile{
 
@@ -60,10 +57,40 @@ mod tests {
     use super::*;
 
     #[test]
-    fn get_repo() {
+    fn get_unet() {
         let model_file = ModelFile::Unet;
         let unet_repo = model_file.get_repo();
+        let unet_path = model_file.get_path(true);
         
         assert_eq!(unet_repo, "stable-diffusion-v1-5/stable-diffusion-v1-5");
+        assert_eq!(unet_path, "unet/diffusion_pytorch_model.fp16.safetensors");
+    }
+
+    #[test]
+    fn get_vae() {
+        let model_file = ModelFile::Vae;
+        let vae_repo = model_file.get_repo();
+        let vae_path = model_file.get_path(false);
+        
+        assert_eq!(vae_repo, "stable-diffusion-v1-5/stable-diffusion-v1-5");
+        assert_eq!(vae_path, "vae/diffusion_pytorch_model.safetensors");
+    }
+    #[test]
+    fn get_tokenizer() {
+        let model_file = ModelFile::Tokenizer;
+        let tokenizer_repo = model_file.get_repo();
+        let tokenizer_path = model_file.get_path(true);
+        
+        assert_eq!(tokenizer_repo, "openai/clip-vit-base-patch32");
+        assert_eq!(tokenizer_path, "tokenizer.json");
+    }
+    #[test]
+    fn get_encoder() {
+        let model_file = ModelFile::Clip;
+        let encoder_repo = model_file.get_repo();
+        let encoder_path = model_file.get_path(true);
+        
+        assert_eq!(encoder_repo, "stable-diffusion-v1-5/stable-diffusion-v1-5");
+        assert_eq!(encoder_path, "text-encoder/model.fp16.safetensors");
     }
 }
