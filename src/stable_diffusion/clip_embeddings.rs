ac46420077fcc25fe8ae4671bb279d9046c9c5f6
Lorenzo Borelli <lorenzo.borelli27@gmail.com>
2024-10-21 17:09:54 +0200

    added function to encode text

--- src/stable_diffusion/clip_embeddings.rs
+++ src/stable_diffusion/clip_embeddings.rs
@@ -34,18 +34,32 @@ fn get_padding_id(tokenizer: &Tokenizer, stable_diffusion_config: &stable_diffus
 
 fn encode_text(text: &str, tokenizer: &Tokenizer, stable_diffusion_config: &stable_diffusion::StableDiffusionConfig, device: &candle_core::Device) -> anyhow::Result<candle_core::Tensor>{
 
-    let mut tokens = tokenizer
+    let tokens = tokenizer
         .encode(text, true)
         .map_err(anyhow::Error::msg)?
         .get_ids()
         .to_vec();
 
     let padding_id = get_padding_id(&tokenizer, &stable_diffusion_config);
-    while tokens.len() < stable_diffusion_config.clip.max_position_embeddings {
-        tokens.push(padding_id)
+    
+    let padded_tokens: Vec<_>;
+    let n_tokens = tokens.len();
+    if  n_tokens <= stable_diffusion_config.clip.max_position_embeddings{
+        padded_tokens = tokens
+        .into_iter()
+        .chain(std::iter::repeat(padding_id)
+            .take(stable_diffusion_config.clip.max_position_embeddings - n_tokens))
+        .collect();
+    } 
+    else {
+        // text too long, panic
+        anyhow::bail!("Prompt is too long ({}), max tokens allowed {}", n_tokens, stable_diffusion_config.clip.max_position_embeddings)
     }
-    let tokens = candle_core::Tensor::new(tokens.as_slice(), device)?.unsqueeze(0)?;
-    Ok(tokens)
+
+
+
+    let encoded_text = candle_core::Tensor::new(padded_tokens.as_slice(), device)?.unsqueeze(0)?;
+    Ok(encoded_text)
 
 }
 
@@ -77,25 +91,50 @@ mod tests {
 
         assert!(tokenizer.is_ok())
     }
-    // static TOKENIZER: anyhow::Result<Tokenizer> = get_tokenizer();
-    #[test]
-    fn test_get_padding_id() -> anyhow::Result<(), String>{
+    // #[test]
+    // fn test_get_padding_id() -> anyhow::Result<(), String>{
 
-        let width = Some(640 as usize);
-        let height: Option<usize> = Some(480 as usize);
-        let sd_config = stable_diffusion::StableDiffusionConfig::v1_5(None, height, width);
-        let tokenizer = get_tokenizer();
+    //     let width = Some(640 as usize);
+    //     let height: Option<usize> = Some(480 as usize);
+    //     let sd_config = stable_diffusion::StableDiffusionConfig::v1_5(None, height, width);
+    //     let tokenizer = get_tokenizer();
 
-        match tokenizer {
-            Ok(tokenizer) => {
-                // assumes padding token in clip config has been set to None
-                let padding_id = get_padding_id(&tokenizer, &sd_config);
-                assert_eq!(padding_id, *tokenizer.get_vocab(true).get(CLIP_SPECIAL_TOKEN).unwrap());
-                Ok(())
-            },
-            Err(_e) => Err(String::from("Error while downloading tokenizer"))
-        }
-    }
+    //     match tokenizer {
+    //         Ok(tokenizer) => {
+    //             // assumes padding token in clip config has been set to None
+    //             let padding_id = get_padding_id(&tokenizer, &sd_config);
+    //             assert_eq!(padding_id, *tokenizer.get_vocab(true).get(CLIP_SPECIAL_TOKEN).unwrap());
+    //             Ok(())
+    //         },
+    //         Err(_e) => Err(String::from("Error while downloading tokenizer"))
+    //     }
+    // }
+
+    // #[test]
+    // fn test_encode_text() -> anyhow::Result<(), String>{
+    //     let text = "First sentence";
+
+    //     let width = Some(640 as usize);
+    //     let height: Option<usize> = Some(480 as usize);
+    //     let sd_config = stable_diffusion::StableDiffusionConfig::v1_5(None, height, width);
+    //     let tokenizer = get_tokenizer();
+
+    //     match tokenizer {
+    //         Ok(tokenizer) => {
+    //             // assumes padding token in clip config has been set to None
+    //             let encoded_text = encode_text(text, &tokenizer, &sd_config, &candle_core::Device::Cpu);
+    //             match encoded_text {
+    //                 Ok(encoded_text) => {
+
+    //                     Ok(())
+    //                 }
+    //                 Err(e) => Err(String::from("Error while encoding text"))
+    //             }
+
+    //         },
+    //         Err(_e) => Err(String::from("Error while downloading tokenizer"))
+    //     }
+    // }
 
 }
 //     #[test]
